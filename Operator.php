4cfa82e9c459fd55d526d909bdcff9656620fc2f
zhoupengzu <zhoupengzu@163.com>
2017-10-29 19:45:03 +0800

    操作符

---
+++ Operator.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>运算符</title>
+  </head>
+  <body>
+    <?php
+      /**
+       * 算数运算符、赋值运算符、递增和递减运算符和其他的一样
+       * <>和!=完全等价，都表示值是否相等
+       */
+
+     ?>
+  </body>
+</html>
