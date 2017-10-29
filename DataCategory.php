447a2fbf2444e93baf4ecb8df011ae0d8b91e050
zhoupengzu <zhoupengzu@163.com>
2017-10-29 19:17:50 +0800

    数据类型

---
+++ DataCategory.php
@@ -0,0 +1,17 @@
+<!--DataCategory.php-->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>数据类型</title>
+  </head>
+  <body>
+    <?php
+      /**
+       * var_dump()函数会返回变量的数据类型和值
+       */
+      $text = "I am a string";
+      var_dump($text);
+     ?>
+  </body>
+</html>
