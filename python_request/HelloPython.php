ec89cbcf342fbdfffe14d6c164a6ed50cea0de96
zhoupengzu <zhoupengzu@maizijf.com>
2017-11-16 14:52:03 +0800

    php commucate with python

---
+++ python_request/HelloPython.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhoupengzu
+ * Date: 2017/11/16
+ * Time: 14:15
+ */
+echo "Hello python";
+//echo json_encode(array("name"=>"zhoupengzu",age=>20));
+//echo json_encode(array(1,2,3,4));
