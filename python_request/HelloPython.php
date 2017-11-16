53e26c119c925198b5c1e6295994825513ee0123
zhoupengzu <zhoupengzu@maizijf.com>
2017-11-16 14:57:21 +0800

    send data to python

--- python_request/HelloPython.php
+++ python_request/HelloPython.php
@@ -5,6 +5,12 @@
  * Date: 2017/11/16
  * Time: 14:15
  */
-echo "Hello python";
+echo "Hello python\n";
 //echo json_encode(array("name"=>"zhoupengzu",age=>20));
 //echo json_encode(array(1,2,3,4));
+$name = $_GET["name"];
+if ($name == "zhoupengzu"){
+    echo "correct user name";
+}else{
+    echo "user name false";
+}
